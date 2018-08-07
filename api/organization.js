b8a52224c259439d0ea9afcbb7dbbcf5743a4ac7
= <reser13@gmail.com>
2018-08-07 17:23:37 -0400

    REP-164 - Added routes for getting / deleting / creating.

---
+++ api/organization.js
@@ -0,0 +1,91 @@
+const Joi = require('joi');
+
+module.exports = [
+    {
+        method: 'POST',
+        path: '/organizations',
+        handler: async (request, h) => {
+            const uow = await request.app.getNewUoW();
+            const logger = request.server.app.logger;
+            const payload = request.payload;
+
+            logger.debug(`Creating organization`);
+
+            const result = await uow.organizationsRepository.createOrganization(payload.name, payload.personal);
+            
+            return result;
+        },
+        options: {
+            auth: false,
+            validate: {
+                payload: {
+                    name: Joi.string().uuid().required(),
+                    personal: Joi.bool().required()
+                }
+            }
+        }  
+    },
+    {
+        method: 'DELETE',
+        path: '/organizations/{id}',
+        handler: async (request, h) => {
+            const uow = await request.app.getNewUoW();
+            const logger = request.server.app.logger;
+            const id = request.params.id;
+
+            logger.debug(`Deleting organization with id: ${id}`);
+
+            const result = await uow.organizationsRepository.deleteOrganization(id);
+            
+            return result;
+        },
+        options: {
+            auth: false,
+            validate: {
+                params: {
+                    id: Joi.string().uuid().required()
+                }
+            }
+        }  
+    },
+    {
+        method: 'GET',
+        path: '/organizations',
+        handler: async (request, h) => {
+            const uow = await request.app.getNewUoW();
+            const logger = request.server.app.logger;
+
+            logger.debug(`Fetching all organizations`);
+
+            const organizations = await uow.organizationsRepository.getAllOrganizations();
+            
+            return organizations;
+        },
+        options: {
+            auth: false
+        }
+    },
+    {
+        method: 'GET',
+        path: '/permissions/{id}',
+        handler: async (request, h) => {
+            const uow = await request.app.getNewUoW();
+            const logger = request.server.app.logger;
+
+            logger.debug(`Fetching organization by id`);
+            const id = request.params.id;
+
+            const organization = await uow.organizationsRepository.getOrganizationById(id);
+            
+            return organization;
+        },
+        options: {
+            auth: false,
+            validate: {
+                params: {
+                    id: Joi.string().guid().required()
+                }
+            }
+        }  
+    }
+];
--- db/repositories/organizationsRepository.js
+++ db/repositories/organizationsRepository.js
@@ -26,6 +26,54 @@ class OrganizationsRepository {
             throw err;
         }
     }
+
+    async deleteOrganization(id) {
+        try {
+            const q = this.uow._models.Organization
+                .query(this.uow._transaction)
+                .patch({deleted: true})
+                .where('id', id);
+
+            const result = await q;
+
+            return result;
+        } catch (err) {
+            this.uow._logger.error(err);
+            this.uow._logger.error(`Failed to delete organization`);
+            throw err;
+        }
+    }
+
+    async getOrganizationById(id) {
+        try {
+            const q = this.uow._models.Organization
+                .query(this.uow._transaction)
+                .where('id', id);
+
+            const organization = await q;
+
+            return organization[0];
+        } catch (err) {
+            this.uow._logger.error(`Failed to fetch organization using id: ${id}`);
+            this.uow._logger.error(err);
+            throw err;
+        }
+    }
+
+    async getAllOrganizations() {
+        try {
+            const q = this.uow._models.Organization
+                .query(this.uow._transaction);
+
+            const organizations = await q;
+
+            return organizations;
+        } catch (err) {
+            this.uow._logger.error(`Failed to fetch organizations`);
+            this.uow._logger.error(err);
+            throw err;
+        }
+    }
 }
 
 module.exports = OrganizationsRepository;
