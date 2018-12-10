1044fea6c0937190f8eb52038646377055495b53
Spencer Dangel <dangelspencer@gmail.com>
2018-12-10 10:35:10 -0500

    REP-307 Update method for setting up test db

--- test/integration/sharedDatabase.js
+++ test/integration/sharedDatabase.js
@@ -1,19 +1,59 @@
+const Model = require('objection').Model;
+const Knex = require('knex');
+
+const defaultConfig = require('../../db/knexfile');
+const UoW = require('../../db');
+const sharedLogger = require('./sharedLogger');
+
 class SharedDatabase {
-    static setUpTest(knex, knexConfig) {
+    static createTestDatabase() {
         return new Promise(async resolve => {
-            await knex.migrate.rollback(knexConfig);
-            await knex.migrate.latest(knexConfig);
-            await knex.seed.run(knexConfig);
+            // get test database name and config
+            const testConfig = JSON.parse(JSON.stringify(defaultConfig));
+            const testDatabaseName = this.getRandomDatabaseName();
+            testConfig.connection.database = testDatabaseName;
+
+            // create test database
+            const tmpKnex = Knex(defaultConfig);
+            await tmpKnex.raw(`CREATE DATABASE ${testConfig.connection.database};`);
+            await tmpKnex.destroy();
+
+            // run migration and seed files on test db
+            const testKnex = Knex(testConfig);
+            await testKnex.migrate.latest(testConfig);
+            await testKnex.seed.run(testConfig);
+
+            // get UoW for test database
+            Model.knex(testKnex);
+            const testUoW = new UoW(sharedLogger);
+            testUoW._knex.destroy();
+            testUoW._knex = testKnex;
+            testUoW._Model = Model;
+
+            resolve({testUoW, testDatabaseName});
+        });
+    }
+
+    static dropTestDatabase(testDatabaseName) {
+        return new Promise(async resolve => {
+            // rollback migrations on test datbase
+            const testConfig = JSON.parse(JSON.stringify(defaultConfig));
+            testConfig.connection.database = testDatabaseName;
+            const testKnex = Knex(testConfig);
+            await testKnex.migrate.rollback(testConfig);
+            await testKnex.destroy();
+
+            // drop test database
+            const tmpKnex = Knex(defaultConfig);
+            await tmpKnex.raw(`DROP DATABASE ${testDatabaseName};`);
+            await tmpKnex.destroy();
+
             resolve();
         });
     }
 
-    static tearDownTest(knex, knexConfig) {
-        return new Promise(async resolve => {
-            await knex.migrate.rollback(knexConfig);
-            await knex.destroy();
-            resolve();
-        });
+    static getRandomDatabaseName() {
+        return `reperio_platform_test_${Math.floor((Math.random() * 100) + 1)}`;
     }
 }
 
