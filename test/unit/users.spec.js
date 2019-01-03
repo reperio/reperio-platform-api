8633af1e024cf2571fe311a661f9f0eaf58a9e22
Andrew Robb <andrewrobb@sevenhillstechnology.com>
2019-01-03 04:14:04 -0500

    REP-366 Added unit testing for checking users by email address

--- test/unit/users.spec.js
+++ test/unit/users.spec.js
@@ -35,17 +35,8 @@ describe('Users API', () => {
     });
 
     describe('GET /users/{emailAddress}/emailExists', () => {
-        it('returns 401 with no permissions', async () => {
-            const options = {
-                url: '/api/users/validEmailAddress@mail.com/emailExists',
-                method: 'GET'
-            };
 
-            const response = await server.server.inject(options);
-            expect(response.statusCode).to.be.equal(401);
-        });
-
-        it('returns 200 with permissions', async () => {
+        it('returns 200 with false', async () => {
             const options = {
                 url: '/api/users/wrongEmailAddress@mail.com/emailExists',
                 method: 'GET'
@@ -53,24 +44,18 @@ describe('Users API', () => {
 
             const response = await server.server.inject(options);
             expect(response.statusCode).to.be.equal(200);
+            expect(response.payload).to.be.equal('false');
         });
 
-        it('returns array of roles', async () => {
+        it('returns 200 with true', async () => {
             const options = {
-                url: '/api/users/invalidAddress/emailExists',
+                url: '/api/users/support@reper.io/emailExists',
                 method: 'GET'
             };
 
             const response = await server.server.inject(options);
             expect(response.statusCode).to.be.equal(200);
-
-            const payload = JSON.parse(response.payload);
-            expect(Array.isArray(payload)).to.be.equal(true);
-            expect(payload[0].id).not.to.be.equal(undefined);
-            expect(payload[0].name).not.to.be.equal(undefined);
-            expect(payload[0].deleted).not.to.be.equal(undefined);
-            expect(payload[0].organizationId).not.to.be.equal(undefined);
-            expect(payload[0].applicationId).not.to.be.equal(undefined);
+            expect(response.payload).to.be.equal('true');
         });
     });
 });
