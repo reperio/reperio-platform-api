99e5aacef527a79c019715e47a6d6504d736f840
Will Osbourne <gitkraken@willosbourne.com>
2019-03-06 16:57:48 -0500

    id:329 adding application id switch for verification email body

--- api/services/emailService.js
+++ api/services/emailService.js
@@ -1,15 +1,25 @@
 class EmailService {
-    async sendVerificationEmail(userEmail, uow, request) {
+    async sendVerificationEmail(userEmail, uow, request, applicationId) {
         const messageHelper = await request.app.getNewMessageHelper();
         const emailVerification = await uow.emailVerificationsRepository.addEntry(userEmail.id, userEmail.userId);
-        const tokenUrl = `${request.server.app.config.webAppUrl}/emailVerification/${emailVerification.id}`
+        const tokenUrl = `${request.server.app.config.webAppUrl}/emailVerification/${emailVerification.id}`;
+        let emailContent = `Please <a href="${tokenUrl}">verify</a> your email address.`
+
+        switch (applicationId) {
+            case '2ce36838-27ae-4c00-b754-e2db7b61c577': // Reperio Managed IT Services
+                emailContent = `Thanks for completing our survey, <a href="${tokenUrl}">click here</a> to go register your first desktop`
+                break;
+        
+            default:
+                break;
+        }
 
         const message = {
             to: userEmail.email,
             from: request.server.app.config.email.sender,
             type: 'email',
             subject: 'Email verification',
-            contents: `Please <a href="${tokenUrl}">verify</a> your email address.`
+            contents: emailContent
         };
 
         return await messageHelper.processMessage(message);
