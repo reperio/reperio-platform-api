cba1d4d1e19ca99a9d8cfeba172764ee06806d6a
Will Osbourne <will@osbourne.org>
2019-05-23 16:16:26 -0400

    id:2865 adding Organization Admin role and adding updateOrganization role

--- db/seeds/4-roles.js
+++ db/seeds/4-roles.js
@@ -14,6 +14,12 @@ exports.seed = async function (knex, Promise) {
             name: 'Inactive Role',
             deleted: true,
             organizationId: '966f4157-934c-45e7-9f44-b1e5fd8b79a7'
+        },
+        {
+            id: 'e37c87b4-b92e-11e8-96f8-529269fb1451',
+            name: 'Organization Admin',
+            deleted: false,
+            organizationId: '966f4157-934c-45e7-9f44-b1e5fd8b79a7'
         }
     ]);
 };
